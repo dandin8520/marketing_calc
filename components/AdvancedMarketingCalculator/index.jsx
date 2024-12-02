5152c2c7d0b19e1b4fc0345fcd7afab8ca177b81
dandin8520 <daniel@advanto.co>
2024-12-02 22:09:53 +0200

    Create index.jsx

---
+++ components/AdvancedMarketingCalculator/index.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Switch } from "@/components/ui/switch";
+import { Label } from "@/components/ui/label";
+import InfoTooltip from '../InfoTooltip';
+import { tooltips } from './tooltips';
+import './styles.css';
