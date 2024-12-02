a68c2fdc2b2a21f7815c6e6c01affa18d2c2a023
dandin8520 <daniel@advanto.co>
2024-12-02 22:10:34 +0200

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdvancedMarketingCalculator from './components/AdvancedMarketingCalculator';
+
+const App = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <AdvancedMarketingCalculator />
+    </div>
+  );
+};
+
+export default App;
